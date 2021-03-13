a104c388455da3d2902a7b41ac73015d1093e666
Staneesh <blkreasons@outlook.com>
2021-03-13 15:09:27 +0100

    Window dimensions updating upon resize

--- src/game.cpp
+++ src/game.cpp
@@ -6,7 +6,10 @@ using namespace sf;
 int run_game(int argc __attribute__((unused)), const char* argv[] __attribute__((unused))) {
     std::cout << "Running game!\n";
 
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	unsigned window_width = 200;
+	unsigned window_height = 200;
+
+	sf::RenderWindow window(sf::VideoMode(window_width, window_height), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
@@ -17,7 +20,15 @@ int run_game(int argc __attribute__((unused)), const char* argv[] __attribute__(
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::Resized)
+			{
+				auto sz = window.getSize();
+				window_width = sz.x;
+				window_height = sz.y;
+			}
         }
+
+		
 
         window.clear();
         window.draw(shape);
