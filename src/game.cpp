d360af22b8b669dd7286206f1605b4a5eb3e2a3a
Staneesh <blkreasons@outlook.com>
2021-03-13 15:52:40 +0100

    Moving the shape

--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <cmath>
+#include <map>
 
 using namespace sf;
 
@@ -13,8 +15,17 @@ int run_game(int argc __attribute__((unused)), const char* argv[] __attribute__(
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+	//TODO(Stanisz): This should be taken from the game state
+	Vector2 position(100.0f, 100.0f);
+	Clock delta_clock;
+	float dt = 0.0f;
+
+	std::map<sf::Keyboard::Key, bool> pressed_keys;
+
     while (window.isOpen())
     {
+		dt = delta_clock.restart().asMilliseconds();
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -32,10 +43,46 @@ int run_game(int argc __attribute__((unused)), const char* argv[] __attribute__(
 				{
 					window.close();
 				}
+
+				pressed_keys[event.key.code] = 1;
+			}
+			if (event.type == sf::Event::KeyReleased)
+			{
+				pressed_keys[event.key.code] = 0;
 			}
         }
 
+		sf::Vector2f moving_dir(0.0f, 0.0f);
+		if (pressed_keys[sf::Keyboard::W])
+		{
+			moving_dir += Vector2f(0.0f, -1.0f);
+		}
+		if (pressed_keys[sf::Keyboard::D])
+		{
+			moving_dir += Vector2f(1.0f, 0.0f);
+		}
+		if (pressed_keys[sf::Keyboard::A])
+		{
+			moving_dir += Vector2f(-1.0f, 0.0f);
+		}
+		if (pressed_keys[sf::Keyboard::S])
+		{
+			moving_dir += Vector2f(0.0f, 1.0f);
+		}
 		
+		float moving_dir_len = (float)moving_dir.x * moving_dir.x + moving_dir.y * moving_dir.y;
+		moving_dir_len = sqrt(moving_dir_len);
+
+		if (moving_dir_len > 0.0f)
+		{
+			moving_dir /= moving_dir_len;
+		}
+
+		float speed = 0.5f;
+		position += speed * dt * moving_dir;
+
+		
+		shape.setPosition(position.x, position.y);		
 
         window.clear();
         window.draw(shape);
